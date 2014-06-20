535deb38598f3750a5b797720d22236a5653c396
blakemerryman <blake.merryman@gmail.com>
2014-06-20 12:39:20 -0500

    Added TableVC class and linked it to storyboard.

---
+++ Net Promoter Score Collector/NPTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NPTableViewController.h
+//  Net Promoter Score Collector
+//
+//  Created by Blake Merryman on 6/20/14.
+//  Copyright (c) 2014 Blake Merryman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NPTableViewController : UITableViewController
+
+@end
